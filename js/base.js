66f044a2513f867ce352bbbc7ac9b13619113ec0
Han-Joon-Hyeok <virq@naver.com>
2020-11-21 22:49:14 +0900

    nav 메뉴 hide/show 기능 추가

--- js/base.js
+++ js/base.js
@@ -1,5 +1,14 @@
-var e = document.getElementById('line-wrapper')
-e.addEventListener('click', function() {
-	if (this.className == 'on') this.classList.remove('on');
-	else this.classList.add('on');
+var burger  = document.getElementById('line-wrapper')
+var menu  = document.getElementById('menu')
+
+burger.addEventListener('click', function() {
+	if (this.className == 'on') {
+        this.classList.remove('on');
+        menu.classList.remove('on');
+    }
+	else {
+        this.classList.add('on');
+        menu.classList.add('on');
+    }
 });
+
